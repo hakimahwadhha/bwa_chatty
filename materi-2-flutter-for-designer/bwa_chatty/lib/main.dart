3b986168c95dc30efebb637d45abdfd4e454e21e
Ninda Argafani <hakimahwadhha>
2023-03-11 10:35:50 +0700

    20230311 removes navigator.push from main.dart

--- materi-2-flutter-for-designer/bwa_chatty/lib/main.dart
+++ materi-2-flutter-for-designer/bwa_chatty/lib/main.dart
@@ -17,6 +17,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Navigator.push(context, MaterialPageRoute<void>(
+//   builder: (BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('My Page')),
+//       body: Center(
+//         child: TextButton(
+//           child: const Text('POP'),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//       ),
+//     );
+//   },
+// ));
 // class MessagesPage extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
