99c53df970dd2536836631d4b936741c9679e584
Ninda Argafani <hakimahwadhha>
2023-03-11 10:24:11 +0700

    202303070958 Fixing the margin

--- materi-1-flutter-mobile-apps/mobile_app_beginer/lib/main.dart
+++ materi-1-flutter-mobile-apps/mobile_app_beginer/lib/main.dart
@@ -19,41 +19,71 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Testing App'),
-          backgroundColor: Colors.indigo[900],
-        ),
+        // appBar: AppBar(
+        //   title: Text('Testing App'),
+        //   backgroundColor: Colors.indigo[900],
+        // ),
         body: SafeArea(
-          child: Container(
-            margin:
-                EdgeInsets.only(left: 25.0, top: 0.0, right: 0.0, bottom: 0.0),
-            padding:
-                EdgeInsets.only(left: 0.0, top: 300.0, right: 0.0, bottom: 0.0),
-            child: Column(
-              children: <Widget>[
-                SimpleShadow(
-                  child: Image.asset(
-                    'assets/images/hsi-logo.png',
-                    width: 100,
+          child: Center(
+            child: Container(
+              margin:
+                  EdgeInsets.only(left: 0.0, top: 0.0, right: 0.0, bottom: 0.0),
+              padding: EdgeInsets.only(
+                  left: 0.0, top: 380.0, right: 0.0, bottom: 0.0),
+              child: Column(
+                children: <Widget>[
+                  SimpleShadow(
+                    child: Image.asset(
+                      'assets/images/hsi-logo.png',
+                      width: 100,
+                    ),
+                    opacity: 0.3,
+                    color: Colors.black,
+                    offset: Offset(3, 3),
+                    sigma: 2,
                   ),
-                  opacity: 0.3,
-                  color: Colors.black,
-                  offset: Offset(3, 3),
-                  sigma: 2,
-                ),
-                Text(
-                  'Ahlan Wa Sahlan',
-                  style: mainHeader,
-                ),
-                Text(
-                  'Belajar ilmu agama sesuai dengan Al-Quran dan As-Sunnah',
-                  style: subHeader,
-                ),
-                Text(
-                  'Jadikanlah ilmu sebagai jembatan menuju surga',
-                  style: subHeader,
-                ),
-              ],
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 30,
+                      bottom: 10,
+                    ),
+                    child: Text(
+                      'Ahlan Wa Sahlan',
+                      style: mainHeader,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 250,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Belajar ilmu agama sesuai dengan',
+                          style: subHeader,
+                        ),
+                        Text(
+                          'Al-Quran dan As-Sunnah',
+                          style: TextStyle(
+                            color: Colors.indigo[900],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 12,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 5,
+                    ),
+                    child: Text(
+                      'Jadikanlah ilmu sebagai jembatan menuju surga',
+                      style: subHeader,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
