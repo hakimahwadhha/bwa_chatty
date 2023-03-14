38ee3cf8f6721035ca72f77d9fc9d818f352e868
Ninda Argafani <hakimahwadhha>
2023-03-15 05:28:02 +0700

    commit hari ini

--- level-02-materi-3-dart/challenge_2.dart
+++ level-02-materi-3-dart/challenge_2.dart
@@ -6,3 +6,15 @@
  * 2. Buatlah sebuah formula untuk menghitung luas lingkaran
  * 3. Buatlah sebuah formula untuk menghitung volume balok
  */
+
+/**
+ * * Jawaban soal no. 1
+ */
+
+void main() {
+  int sisi = 5;
+  int keliling = sisi * 4;
+
+  print('Keliling persegi yang panjang sisinya adalah 5:');
+  print(keliling);
+}
