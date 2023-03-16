4a4a8da91779c42a6a9ce1648ea48c0ecb424277
Ninda Argafani <hakimahwadhha>
2023-03-17 03:09:36 +0700

    202303170308 new app quiz level 02

---
+++ level-02-materi-3-dart/coba_c3.dart
@@ -0,0 +1,12 @@
+/**
+ * * Sistem Penilaian Hasil Belajar (0 - 100)
+ *  # 91-100 : Sangat Baik
+ *  # 81-90  : Baik
+ *  # 71-80  : Cukup
+ *  # 61-70  : Kurang
+ *  # 0-60   : Sangat Kurang
+ *  apabila nilai < 0 dan > 100 : Nilai invalid
+ * Buatlah decision making sistem penilaian diatas menggunakan if else dan ternary operator!
+ * 
+ */
+
