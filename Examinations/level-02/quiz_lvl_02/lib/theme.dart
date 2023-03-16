c1842d83b25f038bf34386c31a9dc2956518eeaf
Ninda Argafani <hakimahwadhha>
2023-03-17 05:47:28 +0700

    202303170547 add theme.dart

--- Examinations/level-02/quiz_lvl_02/lib/theme.dart
+++ Examinations/level-02/quiz_lvl_02/lib/theme.dart
@@ -1,3 +1,27 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+
+TextStyle titleStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 22,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle subTitleOpenStyle = TextStyle(
+  color: Colors.green,
+  fontSize: 12,
+  fontWeight: FontWeight.w300,
+);
+
+TextStyle subTitleCloseStyle = TextStyle(
+  color: Colors.blueAccent.shade700,
+  fontSize: 12,
+  fontWeight: FontWeight.w300,
+);
+
+TextStyle subTitleSoalStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 12,
+  fontWeight: FontWeight.w300,
+);
