bc5dc152f32a2c24322feca082472ccf3aa15fee
Ninda Argafani <hakimahwadhha>
2023-03-17 05:48:28 +0700

    202303170547 add title and tab menu

--- Examinations/level-02/quiz_lvl_02/lib/program_reguler.dart
+++ Examinations/level-02/quiz_lvl_02/lib/program_reguler.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_lvl_02/theme.dart';
+import 'package:flutter/cupertino.dart';
 
 class ProgramReguler extends StatelessWidget {
   const ProgramReguler({super.key});
@@ -9,7 +11,13 @@ class ProgramReguler extends StatelessWidget {
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.indigo.shade800,
-        leading: addLeadingIcon(),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+          onPressed: () {},
+        ),
+        titleSpacing: 0,
         title: Center(
           child: Container(
             margin: EdgeInsets.all(0.0),
@@ -21,7 +29,7 @@ class ProgramReguler extends StatelessWidget {
                 ),
                 Spacer(),
                 Padding(
-                  padding: const EdgeInsets.only(top: 6.0),
+                  padding: const EdgeInsets.only(top: 6.0, right: 20.0),
                   child: Text(
                     'v.2211-2402',
                     style: TextStyle(
@@ -34,26 +42,59 @@ class ProgramReguler extends StatelessWidget {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text('Silsilah Ilmiyyah 7: Beriman kepada Kitab - Kitab Allah',
+            style: titleStyle),
+      ),
+      // TODO: this part should be in tab menu
+      // !FIXING: the tab menu
+      // Container(
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.stretch,
+      //     children: <Widget>[
+      //       DefaultTabController(
+      //         length: 3,
+      //         initialIndex: 0,
+      //         child: TabBar(
+      //           labelColor: Colors.indigo.shade800,
+      //           unselectedLabelColor: Colors.grey,
+      //           tabs: [
+      //             Tab(
+      //               text: 'Tab1',
+      //             ),
+      //             Tab(
+      //               text: 'Tab2',
+      //             ),
+      //             Tab(
+      //               text: 'Tab3',
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
 
-Widget addLeadingIcon() {
-  return new Container(
-    height: 25.0,
-    width: 25.0,
-    padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-    margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-    child: Stack(
-      alignment: AlignmentDirectional.centerStart,
-      children: <Widget>[
-        IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-          ),
-          onPressed: () {},
-        ),
-      ],
-    ),
-  );
-}
+// Widget addLeadingIcon() {
+//   return new Container(
+//     height: 25.0,
+//     width: 25.0,
+//     padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+//     margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+//     child: Stack(
+//       alignment: AlignmentDirectional.centerStart,
+//       children: <Widget>[
+//         IconButton(
+//           icon: Icon(
+//             Icons.arrow_back,
+//           ),
+//           onPressed: () {},
+//         ),
+//       ],
+//     ),
+//   );
+// }
